607ccb78a54e202bdd791f115b9dd7f3ff8c2156
Sofiia Boldeskul <97964178+sofiiacomputerscience@users.noreply.github.com>
2023-06-15 13:11:07 +0200

    Delete RecipeFinder directory

--- RecipeFinder/relevant_recipes_page.py
+++
@@ -1,7 +0,0 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel
-
-class RelevantRecipesPage(QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        layout = QVBoxLayout(self)
-        layout.addWidget(QLabel("Relevant Recipes Page", self))
