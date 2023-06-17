e7ac7af8e5ce700339795271979e3b259b6e1758
vladshakhtar <stolyarovs2003@gmail.com>
2023-06-18 00:14:32 +0300

    Initial Commit

---
+++ Recipe Finder/Recipe_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_FinderApp.swift
+//  Recipe Finder
+//
+//  Created by Vladislav Stolyarov on 18.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
